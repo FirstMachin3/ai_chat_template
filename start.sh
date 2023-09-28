46fb75c63459c6af804c71902161a7bbe7b566b8
Bragi <143753725+FirstMachin3@users.noreply.github.com>
2023-09-28 13:06:11 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,14 +6,10 @@ python3 -m venv env
 # Activate the virtual environment
 source env/bin/activate
 
-#Install requirements
+#Install Dependencies
 pip install flask
 pip install openai
 pip install gunicorn
 
 # Start App
 gunicorn -b 0.0.0.0:5000 app:app
-
-
-
-
